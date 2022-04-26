d13e645039f62a66bbf5ce1c8540d886a1ea179e
LFC_dorm <1240902193@qq.com>
2022-04-26 19:49:39 +0800

    添加按钮

---
+++ src/main/java/gaozhi/online/ugui/core/activity/widget/UButton.java
@@ -0,0 +1,18 @@
+package gaozhi.online.ugui.core.activity.widget;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author LiFucheng
+ * @version 1.0
+ * @description: TODO 重绘按钮
+ * @date 2022/4/22 18:50
+ */
+public class UButton extends JButton {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
